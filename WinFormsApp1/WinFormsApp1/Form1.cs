6622592b24ff9a7814efc25d0f67da1e9749df6c
thuylinh041299 <thuylinh041299@users.noreply.github.com>
2021-03-22 10:59:16 +0700

    cccc

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int tong = tonghaiso(1, 1);
+        }
+        public int tonghaiso(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
